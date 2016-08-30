4be156f6dd83ec278da1582a6853e60d321a1678
李林凯 <lilinkai1990@gmail.com>
2016-08-30 18:40:43 +0800

    1

--- CSTV/Home/HomeVC.swift
+++ CSTV/Home/HomeVC.swift
@@ -11,22 +11,27 @@ import UIKit
 class HomeVC: UIViewController, UIViewControllerTransitioningDelegate,UINavigationControllerDelegate {
 
     @IBOutlet var contentCollectionView: UICollectionView!
+    
+    let interactivePopTransition:UIPercentDrivenInteractiveTransition = UIPercentDrivenInteractiveTransition()
+    
+    var progress:CGFloat = 0.0
+    
+    var lastContentOffset:CGFloat = 0.0
 
     let pushAnimator = VCAnimatedTransitioning()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.navigationController?.delegate = self
-        
+
         automaticallyAdjustsScrollViewInsets = false
-      
         
         configContentCollectionView()
+        
         // Do any additional setup after loading the view.
     }
     
     override func viewWillLayoutSubviews() {
+        self.navigationController?.delegate = self
         super.viewWillLayoutSubviews()
         configNavStyle()
     }
@@ -36,12 +41,6 @@ class HomeVC: UIViewController, UIViewControllerTransitioningDelegate,UINavigati
         // Dispose of any resources that can be recreated.
     }
 
-}
-
-extension HomeVC{
-    func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning?{
-        return pushAnimator
-    }
 }
 
 extension HomeVC{
@@ -88,6 +87,16 @@ extension HomeVC{
 }
 
 extension HomeVC{
+    func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning?{
+        return pushAnimator
+    }
+    
+    func navigationController(navigationController: UINavigationController, interactionControllerForAnimationController animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactivePopTransition
+    }
+}
+
+extension HomeVC{
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
         return 20
@@ -101,8 +110,41 @@ extension HomeVC{
     
     func configContentCollectionView() {
         contentCollectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "UICollectionViewCell")
+        
+        
+            
+        
     }
-   
+ 
+    func scrollViewWillBeginDragging(scrollView: UIScrollView){
+        
+        lastContentOffset = scrollView.contentOffset.y
+        
+        if scrollView.contentOffset.y == 0 {
+            let homeVC = HomeVC()
+            self.navigationController?.pushViewController(homeVC, animated: true)
+            print("开始拖动")
+        }
+    }
+
+    func scrollViewDidScroll(scrollView: UIScrollView){
+        if (lastContentOffset < scrollView.contentOffset.y) {
+            print("向上滚动");
+        }else{
+            print("向下滚动");
+            if scrollView.contentOffset.y <= 0 {
+                progress = scrollView.contentOffset.y/contentCollectionView.frame.height
+                scrollView.setContentOffset(CGPointZero, animated: false)
+                interactivePopTransition.updateInteractiveTransition(-progress)
+            }
+        }
+    }
+    
+    func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>){
+        interactivePopTransition.cancelInteractiveTransition()
+
+    }
+
 }
 
 
